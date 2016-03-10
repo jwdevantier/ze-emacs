f0f7148d844196728db7bbc055d9cdf242763699
pseudo <Jesper Wendel Devantier>
2016-03-10 15:36:47 +0100

    cleanup & license

--- modules/ze-pkg.el
+++ modules/ze-pkg.el
@@ -25,29 +25,6 @@
 	  (setq -ze-pkglist-refreshed t)))
       (package-install p))))
 
-;; include local module name
-(defmacro ze-mod-load-old (mod-name)
-  "load module"
-  (let ((mn (format "ze-%s" mod-name)))
-    (let ((init/fn (format "%s/init" mn)))
-      `(progn
-	 (load-path/with-dir "~/.emacs.d/modules"
-			     (require (intern ,mn)))
-	 (let ((deps/lst (intern (format "%s/deps" ,mn))))
-	   (when (boundp deps/lst)
-	     (message (format "is list?: %s" (listp deps/lst)))
-	     (message (format "is string?: %s" (stringp deps/lst)))
-	     (message (format "is symbol?: %s" (symbolp deps/lst))) ;;true
-	     (message (format "val: '%s'" deps/lst))
-	     (message (format "sym cmp: '%s'" 'ze-fsharp/deps))
-	     (dolist (d "ssas")
-	       (message (symbol-name d)))))
-	   ;;(setq -ze-mod-deps-lst (append -ze-mod-deps-lst ,(intern-soft ,deps/lst)))
-	   ;;(message (format "%s: dependencies detected" ,mn)))
-	 (when (fboundp (intern-soft ,init/fn))
-	   (add-to-list '-ze-mod-init-fns (intern-soft ,init/fn))
-	   (message (format "%s: initialization fn found" ,mn)))))))
-
 (defmacro ze-mod-load (mod-name)
   "load module"
   (let ((mn (format "ze-%s" mod-name)))
