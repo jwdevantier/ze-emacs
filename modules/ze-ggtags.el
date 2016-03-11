a462539cf31b67d6174772cdc45ab922c83b7aba
pseudo <Jesper Wendel Devantier>
2016-03-11 14:17:54 +0100

    ggtags: fix bug, settings weren't applied

--- modules/ze-ggtags.el
+++ modules/ze-ggtags.el
@@ -19,13 +19,13 @@
 ;    M-,p   -- change to previous match
 
 ;Next 2 functions collectively allow incremental GTAGS updating
-(defun gtags-root-dir ()
+(defun ggtags-root-dir ()
   "returns GTAGS root dir or nil if it doesn't exist"
   (with-temp-buffer
     (if (zerop (call-process "global" nil t nil "-pr"))
 	(buffer-substring (point-min) (1- (point-max))))))
 
-(defun gtags-update-hook ()
+(defun ggtags-update-hook ()
   "if GTAGS root dir has been found, do incremental
   updates of tag files when executed (install as
   'after-save-hook' when 'ggtags-mode'!)"
@@ -34,22 +34,19 @@
 (defun ze-ggtags/init ()
   ;; autoload - only load in module once something else references it
   ;; (in this case - it should be triggrered by a major mode's (e.g. Java's)
-  ;; hook
-  (autoload 'ggtags-mode "ggtags" "Minor mode for exuberant tags")
+  ;; expect others to load this
+  ;; hence attach everything to the ggtags-mode-hook
+  (defun ze-ggtags-mode-defaults ()
+    "Default settings for ggtags"
+    ;; enable incremental updating of tag files (on file save)
+    (add-hook 'after-save-hook #'ggtags-update-hook)
 
-  (eval-after-load 'ggtags-mode
-    '(progn
-       (defun ze-ggtags-mode-defaults ()
-	 "Default settings for ggtags"
-	 ;; enable incremental updating of tag files (on file save)
-	 (add-hook 'after-save-hook #'ggtags-update-hook)
+    (define-key ggtags-mode-map (kbd "M-\-") 'ggtags-find-reference)
+    (define-key ggtags-mode-map (kbd "M-\,") 'pop-tag-mark))
 
-	 (define-key ggtags-mode-map (kbd "M-\-") 'ggtags-find-reference)
-	 (define-key ggtags-mode-map (kbd "M-\,") 'pop-tag-mark)
-	 (message "ggtags triggered"))
-
-       (setq ze-ggtags-mode-hook 'ze-ggtags-mode-defaults)
-       (add-hook 'ggtags-mode-hook
-		 (lambda () (run-hooks 'ze-ggtags-mode-hook))))))
+  (setq ze-ggtags-mode-hook 'ze-ggtags-mode-defaults)
+  (add-hook 'ggtags-mode-hook
+	    (lambda ()
+	      (run-hooks 'ze-ggtags-mode-hook))))
 
 (provide 'ze-ggtags)
